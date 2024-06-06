ab389cd5abaa1b52b72156e9aadde1dd3ade5a5b
patlikespython <patryktolkin9@gmail.com>
2024-06-06 11:19:15 +0000

    new

--- exercises/exercise_16.py
+++ exercises/exercise_16.py
@@ -1,3 +1,7 @@
 # Your solution to Exercise 16
+day=int(input("Enter a day:    "))
+month=int(input("Enter a month:   "))
+year=int(input("Enter a year:     "))
 
+check_leap = (year%4==0 and year%100!=0) or (year%400==0)
 
